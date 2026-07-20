1bfe79b5506056b4d5854854f28d401534f461e8
cobachie <euphemiaobachie@gmail.com>
2026-07-20 11:09:07 +0100

    first commit

---
+++ Variable/another_variable.py
@@ -0,0 +1,10 @@
+favorite_food = "Rice"
+print ("My favorite food is", favorite_food)
+favorite_color = "Blue"
+print ("My favorite color is", favorite_color) 
+dream_job = "Software Developer"
+print ("My dream job is", dream_job)
+height = 5.8
+print ("My height is", height)
+is_student = True
+print ("Am I a student?", is_student)
