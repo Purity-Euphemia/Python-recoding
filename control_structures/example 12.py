c979c5eed46c00d0db728849d6980b2e2013d270
cobachie <euphemiaobachie@gmail.com>
2026-07-27 15:56:45 +0100

    Commiting Purity

---
+++ control_structures/example 12.py
@@ -0,0 +1,7 @@
+num = int(input("Enter a number: " ))
+if num >= 18:
+    print("You are an adult.")
+elif num == 18:
+    print("You are exactly 18 years old.")
+else:
+    print("You are a minor.")
