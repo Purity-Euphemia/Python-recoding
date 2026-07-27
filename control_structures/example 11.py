1e5c969c31806443a248a6b796c26fd0f9629299
cobachie <euphemiaobachie@gmail.com>
2026-07-27 15:20:57 +0100

    Commiting Purity

--- control_structures/example 11.py
+++ control_structures/example 11.py
@@ -1,5 +1,7 @@
 for number in range(1, 11):
     if number % 2 == 0:
         print("Even number:", number, "is even")
+    elif number % 2 != 0:
+        print("Prime number:", number, "")    
     else:
         print("Odd number:", number, "is odd")
