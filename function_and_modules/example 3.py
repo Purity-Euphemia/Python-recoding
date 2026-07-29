ef7766bd7979432f9049a1d289b747341063cfd1
cobachie <euphemiaobachie@gmail.com>
2026-07-29 11:22:44 +0100

    my work

---
+++ function_and_modules/example 3.py
@@ -0,0 +1,6 @@
+def describe_pet(pet_name, animal_type='dog'):
+    print(f"I have a {animal_type} named {pet_name}.")
+
+    describe_pet(pet_name='Rex')
+
+    describe_pet(pet_name='Whiskers', animal_type='cat')
