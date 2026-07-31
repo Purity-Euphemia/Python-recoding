6eebc07e136fcdb56dd01dbf75a2c5732c4ee375
cobachie <euphemiaobachie@gmail.com>
2026-07-31 15:45:54 +0100

    my work

---
+++ exception_handling/example 4.py
@@ -0,0 +1,15 @@
+class NegativeAgeError(Exception):
+    """Exception raised when the age is negative."""
+    def __init__(self, age):
+        self.message = f"Age {age} is not valid. A ge cannot be negative."
+        super().__init__(self.message)
+
+def check_age(age):
+        if age < 0:
+            raise NegativeAgeError(age)
+        print(f"Age {age} is valid.")
+
+try:
+     check_age(-5)
+except NegativeAgeError as e:
+    print("Error:", e)
