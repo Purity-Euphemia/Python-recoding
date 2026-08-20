8f5607d3ea62878deee4e255a164474371c372a2
Purity-Euphemia <euphemiaobachie@gmail.com>
2026-08-20 12:18:11 +0100

    my work

---
+++ data_structures/list/example 5.py
@@ -0,0 +1,15 @@
+
+fruits = ["apple", "banana", "cherry"]
+print("Original list:", fruits)
+
+# Adding an element to the end of the list
+fruits.append("Orange")
+print("After appending:", fruits)
+
+#Inserting an element at a specific position
+fruits.insert(1, "blueberry")
+print("After inserting:", fruits)
+
+# Removing an element from the list
+fruits.remove("banana")
+print("After removing:", fruits)
