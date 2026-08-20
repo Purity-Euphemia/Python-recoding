1f109dc193c330f68e68735c5d3597ca6ae18b8f
Purity-Euphemia <euphemiaobachie@gmail.com>
2026-08-20 10:03:37 +0100

    my work

--- workingWithFile/example 2.py
+++ workingWithFile/example 2.py
@@ -1,3 +1,6 @@
 with open('example.txt', 'w') as file:
     file.write("Hello, python!\n")
     file.write("Writing to files is essential.")
+
+with open('example.txt', 'r') as file:
+    file.write("\nAppending a new line to the file.")  # This will raise an error because the file is opened in read mode
