581ec9947e4c3f51ba0b91b38f6ef91d7d026760
Purity-Euphemia <euphemiaobachie@gmail.com>
2026-08-20 12:16:13 +0100

    my work

---
+++ data_structures/list/example 3.py
@@ -0,0 +1,11 @@
+
+fruits = ["apple", "banana", "cherry"]
+print("Original list:", fruits)
+
+# Adding an element to the end of the list
+fruits.append("Orange")
+print("After appending:", fruits)
+
+#Inserting an element at a specific position
+fruits.insert(1, "blueberry")
+print("After inserting:", fruits)
