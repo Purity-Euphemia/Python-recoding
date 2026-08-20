205bb69bfee28b8e0d620b0e387c10c098032cb3
Purity-Euphemia <euphemiaobachie@gmail.com>
2026-08-20 11:19:14 +0100

    my work

--- workingWithFile/example 4.py
+++ workingWithFile/example 4.py
@@ -5,3 +5,12 @@ data = {
     "age": 28,
     "city": "New York"
 }
+
+# Writing JSON to a file
+with open('data.json', 'w') as file:
+    json.dump(data, file)
+
+# Reading JSON from a file
+with open('data.json', 'r') as file:
+    data = json.load(file)
+    print(data)
