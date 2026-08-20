a99a6932d7b2f4ae2a5b3badfa4532e40d764c57
Purity-Euphemia <euphemiaobachie@gmail.com>
2026-08-20 10:16:14 +0100

    my work

--- workingWithFile/example 3.py
+++ workingWithFile/example 3.py
@@ -2,3 +2,4 @@ with open('example.csv', 'w', newline='') as file:
     writer = csv.writer(file)
     writer.writerow(["Name", "Age"])
     writer.writerow(["Alice", 30])
+    writer.writerow(["Bob", 25])
