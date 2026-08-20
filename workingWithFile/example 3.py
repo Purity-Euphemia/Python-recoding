0246d54e3601a5601c1ed10f14f6ca2ab5e0ed0e
Purity-Euphemia <euphemiaobachie@gmail.com>
2026-08-20 11:01:11 +0100

    my work

--- workingWithFile/example 3.py
+++ workingWithFile/example 3.py
@@ -7,3 +7,9 @@ with open('example.csv', 'w', newline='') as file:
     writer.writerow(["Name", "Age"])
     writer.writerow(["Alice", 30])
     writer.writerow(["Bob", 25])
+
+# Reading from a CSV file
+with open('example.csv', 'r') as file:
+    reader = csv.reader(file)
+    for ro in reader:
+        print(row)
