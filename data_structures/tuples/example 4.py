c2be31a0a4433001089ffc0dfb533d70655bb587
Purity-Euphemia <euphemiaobachie@gmail.com>
2026-09-01 14:08:30 +0100

    origin main

--- data_structures/tuples/example 4.py
+++ data_structures/tuples/example 4.py
@@ -1,3 +1,4 @@
 # Tuples can be used as keys in dictionaries, where lists cannot
-color_preferences = (colors: "John's favorite colors")
+colors = ("red", "green", "blue")
+color_preferences = {colors: "John's favorite colors"}
 print(color_preferences)
