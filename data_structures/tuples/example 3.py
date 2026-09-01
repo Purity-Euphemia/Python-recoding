28b36c4de4d110a1fee786c2f8bfc3f3a62a740f
Purity-Euphemia <euphemiaobachie@gmail.com>
2026-09-01 13:19:59 +0100

    origin main

---
+++ data_structures/tuples/example 3.py
@@ -0,0 +1,5 @@
+# Tuples are immutable, so you cannot change their elements
+
+colors = ("red", "green", "blue")
+# Attempting to change the first element will raise an error
+# colors[0] = "yellow"  # This will raise a TypeError   
