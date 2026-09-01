31323d8e611d91b1f89d74e504cbd8567e44cc78
Purity-Euphemia <euphemiaobachie@gmail.com>
2026-09-01 14:45:34 +0100

    origin main

---
+++ data_structures/sets/example 4.py
@@ -0,0 +1,4 @@
+# Checking membership
+numbers = {1, 2, 3, 4, 5}
+if 3 in numbers:
+    print("3 is in the set")
---
+++ data_structures/sets/example 5.py
@@ -0,0 +1,6 @@
+# Operations like union, intersection, difference
+a = {1, 2, 3}
+b = {3, 4, 5}
+print("Union:", a | b)
+print("Intersection:", a & b)
+print("Difference:", a - b)
