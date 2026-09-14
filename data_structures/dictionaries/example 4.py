d359f23dfcc77fce1d2be78e2e83699ca217b29a
Purity-Euphemia <euphemiaobachie@gmail.com>
2026-09-14 11:57:24 +0100

    my work

--- data_structures/dictionaries/example 4.py
+++ data_structures/dictionaries/example 4.py
@@ -1,5 +1,6 @@
-def student(name1="ada"):
-    return f"The students are: {name1}"
-
-y = student("aya")
-print(y)
+#Removing a key-value pair
+person = {"name": "Alice",
+    "age": 30,
+    "city": "New York"}
+del person["age"]
+print("After removing:", person)
