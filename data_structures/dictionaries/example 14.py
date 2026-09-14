3ebcb893d8ee1586a6b432d1913f54c4b5ca16d1
Purity-Euphemia <euphemiaobachie@gmail.com>
2026-09-14 15:01:00 +0100

    my work

---
+++ data_structures/dictionaries/example 14.py
@@ -0,0 +1,13 @@
+# A dictionary about a student
+
+student = {
+    "name": "John",
+    "age": 20,
+    "department": "Computer Science",
+    "courses": ["Data Structures", "Algorithms", "Databases"],
+    "level": 300
+}
+
+print(student["name"])
+print(student["department"])
+print(student["level"])
