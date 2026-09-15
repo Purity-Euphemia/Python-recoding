1484a14c73e876976410d6fcf118662ea0d194c8
Purity-Euphemia <euphemiaobachie@gmail.com>
2026-09-15 15:01:05 +0100

    my work

--- object_oriented_programming/example 1.py
+++ object_oriented_programming/example 1.py
@@ -2,5 +2,6 @@ class Student:
     def __init__(self, name, age):
         self.name = name
         self.age = age
-      
-        
+
+    def study(self):
+        print(f"{self.name} is studying.")
