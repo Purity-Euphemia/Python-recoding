08218ece9d2c5ac0326894803580f50216721ee5
Purity-Euphemia <euphemiaobachie@gmail.com>
2026-09-15 11:22:41 +0100

    my work

---
+++ data_structures/dictionaries/example 16.py
@@ -0,0 +1,13 @@
+# Dictionary inside a Dictionary
+students= {
+    "student1": {
+        "name": "John",
+        "age": 20
+    },
+    "student2": {
+        "name": "Jane",
+        "age": 22
+    }
+}
+
+print(students["student1"]["name"])
