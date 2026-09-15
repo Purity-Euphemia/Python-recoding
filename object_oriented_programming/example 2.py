ef8354a798d47813a66c378a08a732551e4d1dba
Purity-Euphemia <euphemiaobachie@gmail.com>
2026-09-15 15:31:23 +0100

    my work

---
+++ object_oriented_programming/example 2.py
@@ -0,0 +1,7 @@
+class Car:
+    def __init__(self, brand, color):
+        self.brand = self.brand
+        self.color = color
+
+    def start_engine(self):
+        print(f"The engine of the {self.make} {self.model} is starting.")
