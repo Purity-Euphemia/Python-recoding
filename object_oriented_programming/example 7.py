5ede34c908319e9cdbcb6b690ea48ea18eb5f764
Purity-Euphemia <euphemiaobachie@gmail.com>
2026-09-17 16:21:31 +0100

    my work

---
+++ object_oriented_programming/example 7.py
@@ -0,0 +1,16 @@
+class Dog:
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def bark(self):
+        print(self.name, "says Woof!")
+
+
+dog1 = Dog("Bingo", 3)
+
+print(dog1.name)
+print(dog1.age)
+
+dog1.bark()
