02bda36dd9e5607f391214d655537dd3a08aa06a
Purity-Euphemia <euphemiaobachie@gmail.com>
2026-09-18 12:38:06 +0100

    my work

--- object_oriented_programming/example 18.py
+++ object_oriented_programming/example 18.py
@@ -2,4 +2,11 @@ class Laptop:
     def __init__(self, brand, ram):
         self.brand = brand
         self.ram = ram
-    
+
+    def start(self):
+        print(self.brand, "laptop is starting")
+
+laptop1 = Laptop("Dell", "16GB")
+print(laptop1.brand)
+print(laptop1.ram)
+laptop1.start()
