61d1170e19070d32d5ab51959cff96dd446a9988
Purity-Euphemia <euphemiaobachie@gmail.com>
2026-09-18 12:18:45 +0100

    my work

--- object_oriented_programming/example 17.py
+++ object_oriented_programming/example 17.py
@@ -2,3 +2,8 @@ class Cat:
     def __init__(self, name, color):
         self.name = name
         self.color = color
+
+    def meow(self):
+        print(self.name, "says Meow!")
+
+cat1 = Cat("Milo", "White")
