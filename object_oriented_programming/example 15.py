0081a0c656d39993daa28d649a37b2e6efb91244
Purity-Euphemia <euphemiaobachie@gmail.com>
2026-09-18 11:50:11 +0100

    my work

--- object_oriented_programming/example 15.py
+++ object_oriented_programming/example 15.py
@@ -2,3 +2,11 @@ class Food:
     def __init__(self, name, price):
         self.name = name
         self.price = price
+
+    def eat(self):
+        print("I am eating", self.name)
+
+food1 = Food("Pizza", 5000)
+
+print(food1.name)
+print(food1.price)
