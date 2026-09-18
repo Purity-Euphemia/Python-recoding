596f442fa5813dc23045d2f433ca8d4c2655e143
Purity-Euphemia <euphemiaobachie@gmail.com>
2026-09-18 10:37:07 +0100

    my work

--- object_oriented_programming/example 13.py
+++ object_oriented_programming/example 13.py
@@ -2,3 +2,6 @@ class Customer:
     def __init__(self, name, queue_number):
         self.name = name
         self.queue_number = queue_number
+
+    def show_ticket(self):
+        print(self.name, "has ticket", self.queue_number)
