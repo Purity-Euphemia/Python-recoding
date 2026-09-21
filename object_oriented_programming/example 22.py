c0206b5b22ca62236f47079ade6b36e129e70a66
Purity-Euphemia <euphemiaobachie@gmail.com>
2026-09-21 15:25:21 +0100

    my work

--- object_oriented_programming/example 22.py
+++ object_oriented_programming/example 22.py
@@ -3,4 +3,17 @@ class BankAccount:
         self.name = name
         self.balance = balance
 
-    
+    def withdraw(self, amount):
+
+        if amount <= self.balance:
+            self.balance = self.balance - amount
+            print("Money withdrawn")
+        else:
+            print("Not enough money")
+
+account1 = BankAccount("Mary", 10000)
+
+account1.withdraw(3000)
+
+print(account1.balance)
+
