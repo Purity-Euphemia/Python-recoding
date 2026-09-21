be83204a7db3038e2a84f495a1213e520886e53d
Purity-Euphemia <euphemiaobachie@gmail.com>
2026-09-21 14:18:27 +0100

    my work

--- object_oriented_programming/example 21.py
+++ object_oriented_programming/example 21.py
@@ -4,13 +4,4 @@ class Book:
         self.title = title
         self.author = author
 
-    def read(self):
-        print("Reading", self.title)
-
-
-book1 = Book("Python for Beginners", "John")
-
-print(book1.title)
-print(book1.author)
-
-book1.read()
+    
